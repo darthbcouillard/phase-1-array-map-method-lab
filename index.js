d1ba14aae11e22a9707a0b9f794ffd5b35a51c6a
brent couillard <brentcouillard@yahoo.com>
2022-11-02 13:12:47 -0500

    commit tree 42c303fc67de993fa1a8c742f14aa5d61babd863

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = () => {
+//   return tutorials.map(tutorial => {
+//    let words = tutorial.split(" ")
+//    return words.map(word => {
+//      return word[0].toUpperCase() + word.slice(1)
+//     }).join(" ")
+//   })
+// }
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => tutorial.split(" ").map(word => word[0].toUpperCase() + word.slice(1)).join(" "))
 }
